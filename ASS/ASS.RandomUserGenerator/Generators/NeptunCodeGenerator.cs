acf539fe3d598ae3cb38c1a0b785c6c7aa8222e9
csikie <58120619+csikie@users.noreply.github.com>
2021-02-20 10:35:07 +0100

    Setup logging

---
+++ ASS/ASS.RandomUserGenerator/Generators/NeptunCodeGenerator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ASS.RandomUserGenerator.Generators
+{
+    public static class NeptunCodeGenerator
+    {
+        private static readonly string charSet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+        private static readonly Random random = new Random();
+
+        public static string GenerateNeptunCode()
+        {
+            int length = 6;
+            StringBuilder builder = new StringBuilder();
+            while (builder.Length < length)
+            {
+                builder.Append(charSet[random.Next(charSet.Length)]);
+            }
+            return builder.ToString();
+        }
+    }
+}
