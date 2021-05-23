ddfb6ae587100b24050f550c8beacbe4a6ae9dbd
csikie <58120619+csikie@users.noreply.github.com>
2021-05-23 17:24:28 +0200

    setup unit test

--- UITest/ASS_AdminTests.cs
+++ UITest/ASS_AdminTests.cs
@@ -1,17 +1,15 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Edge;
+using System.Configuration;
 using System.IO;
 using System.Reflection;
 
 namespace UITest
 {
     [TestClass]
-    public class EdgeDriverTest
+    public class ASS_AdminTests
     {
-        // In order to run the below test(s), 
-        // please follow the instructions from http://go.microsoft.com/fwlink/?LinkId=619687
-        // to install Microsoft WebDriver.
 
         private EdgeDriver _driver;
 
@@ -23,19 +21,19 @@ namespace UITest
             {
                 PageLoadStrategy = PageLoadStrategy.Normal
             };
-            _driver = new EdgeDriver(@"C:\Users\Csiki Erik\Desktop\ASS\UITest\bin\Debug\netcoreapp2.1", options);
+            _driver = new EdgeDriver(ConfigurationManager.AppSettings["DriverPath"], options);
+            _driver.Url = ConfigurationManager.AppSettings["Url"];
         }
 
         [TestMethod]
         public void VerifyPageTitle()
         {
             // Replace with your own test logic
-            _driver.Url = "https://localhost:44303/";
             _driver.FindElementById("Username").SendKeys("admin");
             _driver.FindElementById("Password").SendKeys("Ab1234");
             _driver.FindElement(By.XPath("//*[@id=\"Username\"]"));
             _driver.FindElementByName("login-btn").Click();
-            Assert.AreEqual("Assignment Supervisor System - Index", _driver.Title);
+            Assert.AreEqual("ASS - Kezdõoldal", _driver.Title);
         }
 
         [TestCleanup]
